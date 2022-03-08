750174a2283011585d2a83234c33e60f7796dcae
ahmad fahrezi <ahmadfahrezi8127@gmail.com>
2022-03-08 18:08:23 +0700

    added total summary

--- script.js
+++ script.js
@@ -1,58 +1,161 @@
-console.log(new Date().toLocaleTimeString());
-console.log(new Date().toLocaleString());
+const items = [
+    {
+        sys: { id: 1 },
+        fields: {
+            flavour: "sirsak",
+        },
+    },
+    {
+        sys: { id: 2 },
+        fields: {
+            flavour: "jeruk",
+        },
+    },
+    {
+        sys: { id: 3 },
+        fields: {
+            flavour: "mangga",
+        },
+    },
+    {
+        sys: { id: 4 },
+        fields: {
+            flavour: "jambu",
+        },
+    },
+    {
+        sys: { id: 5 },
+        fields: {
+            flavour: "leci",
+        },
+    },
+    {
+        sys: { id: 6 },
+        fields: {
+            flavour: "stroberi",
+        },
+    },
+    {
+        sys: { id: 7 },
+        fields: {
+            flavour: "original",
+        },
+    },
+];
 
-function decrement() {
-	const number = document.querySelector(".number");
+function createItem() {
+    const menuItems = document.querySelector(".menu_items");
 
-	if (parseInt(number.innerHTML) > 1) {
-		number.innerHTML = parseInt(number.innerHTML) - 1;
-	}
-}
+    const time = new Date().toLocaleTimeString();
+    const date = new Date().toLocaleDateString();
 
-function increment() {
-	const number = document.querySelector(".number");
+    let result = "";
 
-	number.innerHTML = parseInt(number.innerHTML) + 1;
-}
-
-const menuItems = document.querySelector('.menu_items');
-
-function createItem(){
-    const item = document.createElement("div");
-	item.classList.add("item");
-
-    const time = new Date().toLocaleTimeString()
-    const date = new Date().toLocaleDateString()
-
-    item.innerHTML = `
-        <h2 class="item_title">es teh - sirsak</h2>
-               <div class="item_dateTime">
+    items.forEach((item) => {
+        result += `
+         <div class="item" data-id="${item.sys.id}">
+            <h2 class="item_title">${item.fields.flavour}</h2>
+                <div class="item_dateTime">
                     <h5 class="item_date">${date}</h5>
                     <h3 class="item_time">${time}</h3>
                 </div>
-        <div class="item_amount">
-            <button class="left" onclick="decrement()">-</button>
-            <span class="number">1</span>
-            <button class="right" onclick="increment()">+</button>
+            <div class="item_amount">
+                <button class="left">-</button>
+                <span class="number" data-id="${item.sys.id}">1</span>
+                <button class="right">+</button>
+            </div>
+            <button class="item_button" data-id="${item.sys.id}">add</button>
         </div>
-        <button class="item_button">add</button>
-    `
+        `;
+    });
 
-    menuItems.appendChild(item)
+    menuItems.innerHTML = result;
 }
 
-createItem()
+createItem();
 
+function updateDateTime() {
+    const itemTimeAll = [...document.querySelectorAll(".item_time")];
+    const time = new Date().toLocaleTimeString();
+    const date = new Date().toLocaleDateString();
 
-function updateTime(){
-    const itemTime = document.querySelector('.item_time');
-    const itemDate = document.querySelector('.item_date')
-    const time = new Date().toLocaleTimeString()
-    const date = new Date().toLocaleDateString()
-
-    itemTime.textContent = time
-    itemDate.textContent = date
-    console.log(time, date)
+    // update time and date
+    itemTimeAll.forEach((itemTime) => {
+        itemTime.textContent = time;
+        itemTime.previousElementSibling.textContent = date;
+    });
 }
 
-setInterval(updateTime, 1000)
+setInterval(updateDateTime, 1000);
+
+function updateAmount() {
+    const leftButtons = [...document.querySelectorAll(".left")];
+    const rightButtons = [...document.querySelectorAll(".right")];
+
+    // decrement amount
+    leftButtons.forEach((leftButton) => {
+        leftButton.addEventListener("click", () => {
+            const number = leftButton.nextElementSibling;
+
+            if (parseInt(number.innerHTML) > 1) {
+                number.innerHTML = parseInt(number.innerHTML) - 1;
+            }
+        });
+    });
+
+    // incriment amount
+    rightButtons.forEach((rightButton) => {
+        rightButton.addEventListener("click", () => {
+            const number = rightButton.previousElementSibling;
+
+            number.innerHTML = parseInt(number.innerHTML) + 1;
+        });
+    });
+}
+
+updateAmount();
+
+function addToTotal() {
+    const addButtons = [...document.querySelectorAll(".item_button")];
+
+    addButtons.forEach((addButton) => {
+        addButton.addEventListener("click", () => {
+            let amount =
+                addButton.previousElementSibling.firstElementChild
+                    .nextElementSibling.innerHTML;
+            let id = addButton.dataset.id;
+            let name =
+                addButton.previousElementSibling.previousElementSibling
+                    .previousElementSibling.innerHTML;
+            console.log(`id: ${id}, amount: ${amount}, name: ${name}`);
+            // return { id: id, amount: amount, name: name };
+
+            const table = document.querySelector(".table");
+            const tableItem = document.createElement("div");
+            tableItem.classList.add("table_item");
+
+            tableItem.innerHTML = `
+                <p class="table_item_qty">${amount}</p>
+                <p class="table_item_name">${name}</p>
+                <p class="table_item_time">${new Date().toLocaleTimeString()}</p>
+                <button class="table_item_delete">x</button>
+            `;
+
+            table.appendChild(tableItem);
+        });
+    });
+}
+
+addToTotal();
+
+// function createTableItem() {
+//     const tableItem = document.createElement("div");
+//     tableItem.classList.add("table_item");
+
+//     tableItem.innerHTML = `
+//         <p class="table_item_qty">${}</p>
+//         <p class="table_item_name">sirsak</p>
+//         <p class="table_item_time">17:00:34 PM</p>
+//         <button class="table_item_delete">x</button>
+//     `;
+// }
