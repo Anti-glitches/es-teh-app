67cf5781169e370784c5c6901613a1ced5e5903a
ahmad fahrezi <ahmadfahrezi8127@gmail.com>
2022-03-08 19:40:52 +0700

    added color marker and better ui for mobile

--- script.js
+++ script.js
@@ -2,43 +2,66 @@ const items = [
     {
         sys: { id: 1 },
         fields: {
-            flavour: "sirsak",
+            flavour: "original",
+            background:
+                "background: rgb(235,127,3); background: radial-gradient(circle, rgba(235,127,3,1) 0%, rgba(161,86,0,1) 100%);     ",
         },
     },
     {
         sys: { id: 2 },
         fields: {
-            flavour: "jeruk",
+            flavour: "jambu",
+            background:
+                "background: rgb(255,113,113); background: radial-gradient(circle, rgba(255,113,113,1) 0%, rgba(65,213,0,1) 100%);",
         },
     },
     {
         sys: { id: 3 },
         fields: {
             flavour: "mangga",
+            background:
+                "background: rgb(255,209,40); background: radial-gradient(circle, rgba(255,209,40,1) 0%, rgba(255,42,42,1) 100%);",
         },
     },
     {
         sys: { id: 4 },
         fields: {
-            flavour: "jambu",
+            flavour: "leci",
+            background:
+                "background: rgb(255,252,239);background: radial-gradient(circle, rgba(255,252,239,1) 0%, rgba(255,95,95,1) 70%);",
         },
     },
     {
         sys: { id: 5 },
         fields: {
-            flavour: "leci",
+            flavour: "jeruk",
+            background:
+                "background: rgb(250,251,63); background: radial-gradient(circle, rgba(250,251,63,1) 0%, rgba(252,152,70,1) 100%);",
         },
     },
     {
         sys: { id: 6 },
         fields: {
-            flavour: "stroberi",
+            flavour: "sirsak",
+            background:
+                "background: rgb(255,243,194); background: radial-gradient(circle, rgba(255,243,194,1) 2%, rgba(13,186,0,1) 64%, rgba(13,186,0,1) 100%);",
         },
     },
     {
         sys: { id: 7 },
         fields: {
-            flavour: "original",
+            flavour: "melon",
+            background:
+                "background: rgb(255,209,23); background: radial-gradient(circle, rgba(255,209,23,1) 24%, rgba(112,240,68,1) 100%);",
+        },
+    },
+    ,
+    {
+        sys: { id: 8 },
+        fields: {
+            flavour: "stroberi",
+            background:
+                "background: rgb(255,244,198); background: radial-gradient(circle, rgba(255,244,198,1) 33%, rgba(240,68,68,1) 100%);",
         },
     },
 ];
@@ -53,11 +76,10 @@ function createItem() {
 
     items.forEach((item) => {
         result += `
-         <div class="item" data-id="${item.sys.id}">
+        <div class="item" data-id="${item.sys.id}">
             <h2 class="item_title">${item.fields.flavour}</h2>
                 <div class="item_dateTime">
-                    <h5 class="item_date">${date}</h5>
-                    <h3 class="item_time">${time}</h3>
+                    <span class="logo" style="${item.fields.background}"></span>
                 </div>
             <div class="item_amount">
                 <button class="left">-</button>
@@ -67,6 +89,21 @@ function createItem() {
             <button class="item_button" data-id="${item.sys.id}">add</button>
         </div>
         `;
+        // result += `
+        //  <div class="item" data-id="${item.sys.id}">
+        //     <h2 class="item_title">${item.fields.flavour}</h2>
+        //         <div class="item_dateTime">
+        //             <h5 class="item_date">${date}</h5>
+        //             <h3 class="item_time">${time}</h3>
+        //         </div>
+        //     <div class="item_amount">
+        //         <button class="left">-</button>
+        //         <span class="number" data-id="${item.sys.id}">1</span>
+        //         <button class="right">+</button>
+        //     </div>
+        //     <button class="item_button" data-id="${item.sys.id}">add</button>
+        // </div>
+        // `;
     });
 
     menuItems.innerHTML = result;
